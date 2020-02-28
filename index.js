c8452ba3eedbd99ce206897207b7b737567ecf6c
John Telban <Jtelban1@gmail.com>
2020-02-28 08:35:12 -0700

    updated

--- index.js
+++ index.js
@@ -4,37 +4,52 @@ const inquirer = require("inquirer");
 
 const questions = [
     {
-        message: "what is your name",
-        answer: "username"
+        message: "Enter your GitHub username",
+        name: "username"
     },
     {
-        message: "What is your location",
-        answer: "location"
+        message: "What's your project's name?",
+        name: "project_name"
     },
     {
-        message: "Tell us about yourself?",
-        answer: "bio"
+        message: "What license?",
+        name: "license"
     }
-
 ];
 
 inquirer
     .prompt(questions)
-    .then(function(answers) {
+    .then(function (answers) {
         const queryUrl = `https://api.github.com/users/${answers.username}`;
         axios.get(queryUrl).then(res => {
-            console.log(res.data)
+            if(res.data.email){
+                answers.email = res.data.email;
+            }
+            if(res.data.avatar_url){
+                answers.avatar_url = res.data.avatar_url;
+            }
+
+            let template = templateApply(answers);
+            writeToFile(template);
         });
     });
 
 
-
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    fs.writeFile(__dirname + '/test_README.md', data, function(err){
+        if(err) throw err;
+        process.exit();
+    });
 }
 
-function init() {
 
+function templateApply(values){
+    let template = fs.readFileSync(`${__dirname}/template/README.md`, 'utf8');
+    let substitution;
+    Object.keys(values).forEach(function(key){
+        substitution = '{{'+ key + '}}';
+        template = template.replace(substitution, values[key]);
+    });
+    return template;
 }
-
-init();
 
---
+++ test_README.md
@@ -0,0 +1,17 @@
+[![Buy me a coffee][buymeacoffee-shield]][buymeacoffee]
+
+ [buymeacoffee]: https://www.buymeacoffee.com/jtelban1
+ [buymeacoffee-shield]: https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png
+
+
+# test
+
+
+
+
+idk
+
+
+
+
+## Description
